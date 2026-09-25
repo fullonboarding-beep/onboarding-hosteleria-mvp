ad8fe80b161c313c09af380a68502a2e3c1864a8
fullonboarding-beep <full.onboarding@gmail.com>
2026-09-25 19:13:40 +0100

    Disable stale service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-const CACHE='onboarding-responsable-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request))));
+self.addEventListener('install',event=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>event.respondWith(fetch(event.request)));
